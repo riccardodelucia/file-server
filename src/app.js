dd5ab8a8b3967be3a70c08871c9cdece149fdf25
riccardo.delucia <riccardo.delucia@fht.org>
2022-11-07 11:58:45 +0100

    disabled rate limit for test in stage

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
 import winston from 'winston';
-import rateLimit from 'express-rate-limit';
+//import rateLimit from 'express-rate-limit';
 import xss from 'xss-clean';
 import hpp from 'hpp';
 
@@ -24,8 +24,8 @@ const corsOrigins = process.env.CORS_ORIGINS_FILESERVER?.split(', ') || '';
 const corsMethods = process.env.CORS_METHODS_FILESERVER?.split(', ') || '';
 const JSONSizeLimit = process.env.JSON_SIZE_LIMIT;
 const uploadMaxFileSize = process.env.UPLOAD_MAX_FILE_SIZE;
-const rateLimitMaxRequests = process.env.RATE_LIMIT_MAX_REQUESTS;
-const rateLimitWindowMs = process.env.RATE_LIMIT_WINDOW_MS;
+//const rateLimitMaxRequests = process.env.RATE_LIMIT_MAX_REQUESTS;
+//const rateLimitWindowMs = process.env.RATE_LIMIT_WINDOW_MS;
 
 // App
 const app = express();
@@ -46,12 +46,12 @@ if (environment === 'development') {
   );
 } else throw new Error(`Wrong ENV_VAR value: ${environment}`);
 
-const limiter = rateLimit({
+/* const limiter = rateLimit({
   max: rateLimitMaxRequests,
   windowMs: rateLimitWindowMs,
   message: 'Too many requests from this IP, please try again in one hour.',
-});
-app.use(limiter);
+}); */
+//app.use(limiter);
 
 app.use(helmet());
 
