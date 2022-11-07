d8496ccb82c71e4ef50588cf35660dbee5fc897c
riccardo.delucia <riccardo.delucia@fht.org>
2022-11-07 16:28:17 +0100

    reenabled rate limiter

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
 import winston from 'winston';
-//import rateLimit from 'express-rate-limit';
+import rateLimit from 'express-rate-limit';
 import xss from 'xss-clean';
 import hpp from 'hpp';
 
@@ -24,8 +24,8 @@ const corsOrigins = process.env.CORS_ORIGINS_FILESERVER?.split(', ') || '';
 const corsMethods = process.env.CORS_METHODS_FILESERVER?.split(', ') || '';
 const JSONSizeLimit = process.env.JSON_SIZE_LIMIT;
 const uploadMaxFileSize = process.env.UPLOAD_MAX_FILE_SIZE;
-//const rateLimitMaxRequests = process.env.RATE_LIMIT_MAX_REQUESTS;
-//const rateLimitWindowMs = process.env.RATE_LIMIT_WINDOW_MS;
+const rateLimitMaxRequests = process.env.RATE_LIMIT_MAX_REQUESTS;
+const rateLimitWindowMs = process.env.RATE_LIMIT_WINDOW_MS;
 
 // App
 const app = express();
@@ -46,12 +46,30 @@ if (environment === 'development') {
   );
 } else throw new Error(`Wrong ENV_VAR value: ${environment}`);
 
-/* const limiter = rateLimit({
+const ms2TimeString = (ms) => {
+  const seconds = Math.floor(ms / 1000);
+
+  const hours = Math.floor(seconds / 3600);
+  const remainderHours = seconds % 3600;
+  const minutes = Math.floor(remainderHours / 60);
+  const remainderMinutes = remainderHours % 60;
+
+  return `${hours} ${hours > 1 || hours === 0 ? 'hours' : 'hour'}, ${minutes} ${
+    minutes > 1 || minutes === 0 ? 'minutes' : 'minute'
+  }, ${remainderMinutes} ${
+    remainderMinutes > 1 || remainderMinutes === 0 ? 'seconds' : 'second'
+  }`;
+};
+
+const limiter = rateLimit({
   max: rateLimitMaxRequests,
   windowMs: rateLimitWindowMs,
-  message: 'Too many requests from this IP, please try again in one hour.',
-}); */
-//app.use(limiter);
+  message: `Too many requests from this IP, please try again in ${ms2TimeString(
+    rateLimitWindowMs
+  )}`,
+});
+
+app.use(limiter);
 
 app.use(helmet());
 
